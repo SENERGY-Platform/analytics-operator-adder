026af02ca69a3b94a09e6526cca0db5bd91b806a
franzmueller <franzmueller@users.noreply.github.com>
2019-07-18 11:02:15 +0200

    improve error handling & add logging

--- src/main/java/Adder.java
+++ src/main/java/Adder.java
@@ -15,13 +15,20 @@
  */
 
 
+import org.infai.seits.sepl.operators.Helper;
 import org.infai.seits.sepl.operators.Message;
 import org.infai.seits.sepl.operators.OperatorInterface;
 
+import java.time.format.DateTimeParseException;
 
 
 public class Adder implements OperatorInterface {
 
+    private boolean debug;
+
+    public Adder(){
+        debug = Helper.getEnv("DEBUG", "false").equalsIgnoreCase("true");
+    }
 
     @Override
     public void run(Message message) {
@@ -30,8 +37,23 @@ public class Adder implements OperatorInterface {
         String timestamp1 = message.getInput("timestamp1").getString();
         String timestamp2 = message.getInput("timestamp2").getString();
 
-        long long1 = DateParser.parseDateMills(timestamp1);
-        long long2 = DateParser.parseDateMills(timestamp2);
+        if(debug){
+            System.out.println("Got values:\n\tvalue1: " + value1 + "\n\ttimestamp1: " + timestamp1
+                    + "\n\tvalue2: " + value2 + "\n\ttimestamp2" + timestamp2);
+        }
+
+        long long1 = 0, long2 = 0;
+
+        try{
+            long1 = DateParser.parseDateMills(timestamp1);
+        } catch (DateTimeParseException e) {
+            System.err.println("Could not parse timestamp1, assume 0");
+        }
+        try{
+            long2 = DateParser.parseDateMills(timestamp1);
+        } catch (DateTimeParseException e) {
+            System.err.println("Could not parse timestamp2, assume 0");
+        }
 
         message.output("timestamp", long1 > long2 ? timestamp1 : timestamp2); //Outputs latest timestamp
         message.output("value", value1 + value2);
